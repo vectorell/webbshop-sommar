17fca16c5e795914955e5fdf723575f6108f099d
vectorell <vlindell@outlook.com>
2023-05-04 23:07:51 +0200

    Started working on admin page. Added base structure for layout for both logged in view and not logged in

--- roligt-och-soligt/src/routes/login/Admin.jsx
+++ roligt-och-soligt/src/routes/login/Admin.jsx
@@ -1,11 +1,72 @@
-import { PageTitle } from "./StyledAdmin"
+import { loginState } from "../../recoil/atom/loginState/loginState"
+import { useRecoilState } from "recoil"
+
+
+/** Styled Components */
+import { InputDiv, PageTitle } from "./StyledAdmin"
+import { Form } from "./StyledAdmin"
+import { InputField } from "./StyledAdmin"
+import { ContentDiv } from "./StyledAdmin"
+import { UserDiv } from "./StyledAdmin"
+import { UserImageDiv } from "./StyledAdmin"
+import { UserImage } from "./StyledAdmin"
+import { UserName } from "./StyledAdmin"
+import { EditIcon } from "./StyledAdmin"
+
+import StaffPer from "../../assets/staff/per.jpeg"
+import editIcon from "../../assets/edit-icon2.png"
+
 
 function Admin() {
-
+    const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState)
 
     return (
         <>
-            <PageTitle> Admin </PageTitle>
+            <PageTitle> Admin  </PageTitle>
+
+            <button onClick={ setIsLoggedIn(!isLoggedIn) }> {isLoggedIn ? 'Logga ut' : 'Logga in' } </button>
+
+            {/* OM INLOGGAD */}
+            {isLoggedIn && (
+                <>  
+                    <p>Användare</p>
+                    <ContentDiv>
+                        <UserDiv>
+                            <UserImageDiv>
+                                <UserImage src={StaffPer} alt="Per"/>
+                            </UserImageDiv>
+                            <UserName> Victor </UserName>
+                            <EditIcon src={editIcon} />
+                        </UserDiv>
+                    </ContentDiv>
+                </>
+            )}
+            
+            
+            
+            { !isLoggedIn &&
+            // OM UTLOGGAD
+            
+            (<>
+                <Form>
+                    <InputDiv>
+                        <p> Användarnamn </p>
+                        <InputField />
+                    </InputDiv>
+
+                    <InputDiv>
+                        <p> Lösenord </p>
+                        <InputField />
+                    </InputDiv>
+
+                    <button> Logga in </button>
+                </Form>
+            </>)
+            
+        
+        }
+
+
         </>
     )
 }
--- roligt-och-soligt/src/routes/login/StyledAdmin.jsx
+++ roligt-och-soligt/src/routes/login/StyledAdmin.jsx
@@ -5,3 +5,77 @@ export const PageTitle = styled.h1`
     margin-bottom: 1em;
     font-size: 3em;
 `
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    row-gap: 1em;
+`
+
+export const InputField = styled.input`
+    padding: 0.5em 1em;
+`
+
+export const InputDiv = styled.div`
+    
+`
+
+export const LoginButton = styled.button`
+    
+`
+
+export const ContentDiv = styled.div`
+    border: 1px solid var(--color-light-sea-green);
+    border-radius: var(--radius-medium);
+    width: 80%;
+    min-height: 15em;
+    box-shadow: var(--shadow-normal)
+`
+
+export const UserDiv = styled.div`
+    border: 1px solid black;
+    max-width: 9em;
+    height: 14em;
+    margin: 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: var(--radius-small);
+    transition: 0.2s;
+    box-shadow: var(--shadow-normal);
+    
+    &:hover {
+        transition: 0.2s;
+        transform: var(--transform-hover);
+        box-shadow: var(--shadow-hover);
+    }
+`
+export const UserImageDiv = styled.div`
+    /* border: 1px dashed gray; */
+    width: 80%;
+    height: 50%;
+    margin-top: 0.5em;
+    overflow: hidden;
+    `
+
+export const UserImage = styled.img`
+    width: 100%;
+`
+
+export const UserName = styled.div`
+    margin: 0.5em;
+    font-size: 1.2em;
+    font-weight: 600;
+`
+
+export const EditIcon = styled.img`
+    margin-top: 1em;
+    height: 15%;
+    transition: 0.2s;
+    padding: 0.1em 1em;
+    border-radius: var(--radius-medium);
+    
+    &:hover {
+        transition: 0.2s;
+        background-color: var(--color-colombia-blue);
+    }
+`
