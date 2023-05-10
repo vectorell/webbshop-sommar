14b4f681846e4838dc9ae9ed8d212c5ff42bd880
vectorell <vlindell@outlook.com>
2023-05-10 18:10:53 +0200

    Fixed responsiveness 320px on start page

--- roligt-och-soligt/src/routes/start/StyledStart.jsx
+++ roligt-och-soligt/src/routes/start/StyledStart.jsx
@@ -16,11 +16,20 @@ export const WelcomeCardDiv = styled.div`
     width: 50%;
     min-height: 40em;
     box-shadow: var(--shadow-normal);   
+
+    @media (max-width: 500px) {
+        width: 90%;
+        min-height: fit-content;
+    }
 `
 export const MainInfoDiv = styled.div`
     /* border: 1px dashed green; */
     display: flex;
     column-gap: 5em;
+
+    @media (max-width: 500px) {
+        flex-direction: column;
+    }
 `
 export const InfoDiv = styled.div`
     /* border: 1px dashed green; */
@@ -43,6 +52,8 @@ export const ProductsButton = styled(NavLink)`
     transition: 0.2s;
     font-size: 1.5em;
     border-radius: var(--radius-small);
+    margin: 1em;
+    text-align: center;
 
     &:hover {
         background: white;
